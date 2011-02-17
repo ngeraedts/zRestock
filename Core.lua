2ea87b08f46aebe3f63fdb28b46d5d8e00b27d8d
ngeraedts@gmail.com <Zxian@.(none)>
2011-02-16 17:29:40 -0800

    first real commit

---
+++ Core.lua
@@ -0,0 +1,93 @@
+local T, C, L = unpack(Tukui)
+
+-------------------------------------
+-------------  OPTIONS  -------------
+-------------------------------------
+
+local verbose = false
+
+if T.myclass == "ROGUE" then
+	items = {
+		[6947]	= 20,		-- Instant Poison
+		[2892]	= 20,		-- Deadly Poison
+		[3775]	= 20,		-- Cripping Poison
+		[5237]	= 20,		-- Mind-Numbing Poison
+		[10918]	= 20,		-- Wound Poison
+	}
+elseif T.myclass == "MAGE" then
+	items = {
+		[17020] = 20,		-- Arcane Powder
+		[17031] = 20, 		-- Rune of Teleportation
+		[17032] = 20,		-- Rune of Portals
+	}
+elseif T.myclass == "SHAMAN" then
+	items = {
+		[17030] = 20,		-- Ankh
+	}
+elseif T.myclass == "DRUID" then
+	items = {
+		[17034] = 20,		-- Maple Seed
+	}
+end
+
+
+-------------------------------------
+------------  TEH STUFF  ------------
+-------------------------------------
+
+
+local function ItemLinkToItemID(itemlink)
+	return tonumber(string.match(itemlink, "item:(%d+)"))
+end
+
+local function TR_GetItemInventoryCount(itemID)
+	count = 0
+	for bag = 0, 4, 1 do
+		for slot = 1, GetContainerNumSlots(bag), 1 do
+			if itemID == GetContainerItemID(bag,slot) then
+				_, slotcount = GetContainerItemInfo(bag,slot)
+				count = count + slotcount
+			end
+		end
+	end
+	return count
+end
+
+
+local function TR_GetMerchantItemIndex(itemID)
+	for i = 1, GetMerchantNumItems() do
+		merchantIL = GetMerchantItemLink(i)
+		local merchantIID = ItemLinkToItemID(merchantIL)
+		if itemID == merchantIID then
+			return i
+		end
+	end
+	return
+end
+
+local function TR_BuyItemFromMerchant(itemIndex,qty,iStackCount)
+	while qty > iStackCount do
+		BuyMerchantItem(itemIndex,iStackCount)
+		qty = qty - iStackCount	
+	end
+	BuyMerchantItem(itemIndex,qty)
+end
+
+local function eventHandler(self, event, ...)
+	for item,stack in pairs(items) do
+		_, itemLink, _, _, _, _, _, iStackCount = GetItemInfo(item)
+		itemID = ItemLinkToItemID(itemLink)
+		invItemCount = TR_GetItemInventoryCount(item)
+		merchantIndex = TR_GetMerchantItemIndex(item)
+		numPurchase = stack - invItemCount
+		if numPurchase > 0 then
+			if verbose then print("Buying "..numPurchase.." "..itemLink) end
+			TR_BuyItemFromMerchant(merchantIndex,numPurchase,iStackCount)
+		end
+		
+	end
+end
+
+local frame = CreateFrame("FRAME", "TinyRestockerFrame");
+frame:RegisterEvent("MERCHANT_SHOW");
+frame:SetScript("OnEvent", eventHandler);
